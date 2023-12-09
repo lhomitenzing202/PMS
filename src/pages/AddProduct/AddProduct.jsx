72e20f86cdd4df5b9919c5bcf873c743f950b898
lhomitenzing202 <tenzinglhomi15@gmail.com>
2023-12-09 22:08:27 +0545

    2nd approach aaded

--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -5,45 +5,79 @@ import axios from "axios"
 
 
 const AddProduct = () => {
-    const[ProductImg,setProductImg]=useState("")
-    const[ProductName,setProductName]=useState("")
-    const[ProductDescription,setProductDescription]=useState("")
-    const[ProductMaterial,setProductMatreial]=useState("")
 
-    const AddProduct = async(e)=>{
-        e.preventDefault()
-        const response = await axios.post("https://65732380192318b7db419d5a.mockapi.io/Product",{
-            ProductImg:ProductImg,
-            ProductName:ProductName,
-            ProductDescription:ProductDescription,
-            ProductMaterial:ProductMaterial
-        })
-        console.log(response)
-    }
+    //First approach to integrate Products
+    // const[ProductImg,setProductImg]=useState("")
+    // const[ProductName,setProductName]=useState("")
+    // const[ProductDescription,setProductDescription]=useState("")
+    // const[ProductMaterial,setProductMatreial]=useState("")
+
+    // const AddProduct = async(e)=>{
+    //     e.preventDefault()
+    //     const response = await axios.post("https://65732380192318b7db419d5a.mockapi.io/Product",{
+    //         ProductImg:ProductImg,
+    //         ProductName:ProductName,
+    //         ProductDescription:ProductDescription,
+    //         ProductMaterial:ProductMaterial
+    //     })
+    //     console.log(response)
+    // }
 
  
-  return (
- <>
- <Navbar />
+//   return (
+//  <>
+//  <Navbar />
 
-<div id="product-form">
- <form onSubmit={AddProduct}>
-        <label htmlFor="productImage">Product Image URL:</label>
-        <input type="text" id="productImage" name="productImage" onChange={(e)=>setProductImg(e.target.value)}/>
+// <div id="Product-form">
+//  <form onSubmit={AddProduct}>
+//         <label htmlFor="ProductImage">Product Image URL:</label>
+//         <input type="text" id="ProductImage" name="ProductImage" onChange={(e)=>setProductImg(e.target.value)}/>
 
-        <label htmlFor="productName">Product Name:</label>
-        <input type="text" id="productName" name="productName" onChange={(e)=>setProductName(e.target.value)}/>
+//         <label htmlFor="ProductName">Product Name:</label>
+//         <input type="text" id="ProductName" name="ProductName" onChange={(e)=>setProductName(e.target.value)}/>
 
-        <label htmlFor="productDescription">Product Description:</label>
-        <textarea id="productDescription" name="productDescription" rows="4" onChange={(e)=>setProductDescription(e.target.value)}></textarea>
+//         <label htmlFor="ProductDescription">Product Description:</label>
+//         <textarea id="ProductDescription" name="ProductDescription" rows="4" onChange={(e)=>setProductDescription(e.target.value)}></textarea>
 
-        <label htmlFor="productMaterial">Product Material:</label>
-        <input type="text" id="productMaterial" name="productMaterial" onChange={(e)=>setProductMatreial(e.target.value)}/><br/>
+//         <label htmlFor="ProductMaterial">Product Material:</label>
+//         <input type="text" id="ProductMaterial" name="ProductMaterial" onChange={(e)=>setProductMatreial(e.target.value)}/><br/>
 
-        <button type="submit">Submit</button>
-    </form>
-    </div>
- </>
+//         <button type="submit">Submit</button>
+//     </form>
+//     </div>
+//  </>
+
+const AddProduct = async(e)=>{
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget);//{}
+     const data = Object.fromEntries(formData)
+    
+     const response = await axios.post("https://65732380192318b7db419d5a.mockapi.io/Product",data)
+     console.log(response)
+    }
+
+return (
+    <>
+    <Navbar />
+   
+   <div id="Product-form">
+    <form onSubmit={AddProduct}>
+           <label htmlFor="ProductImage">Product Image URL:</label>
+           <input type="text" id="ProductImg" name="ProductImg" required />
+   
+           <label htmlFor="ProductName">Product Name:</label>
+           <input type="text" id="ProductName" name="ProductName" required/>
+   
+           <label htmlFor="ProductDescription">Product Description:</label>
+           <textarea id="ProductDescription" name="ProductDescription" rows="4" required></textarea>
+   
+           <label htmlFor="ProductMaterial">Product Material:</label>
+           <input type="text" id="ProductMaterial" name="ProductMaterial" required/><br/>
+   
+           <button type="submit">Submit</button>
+       </form>
+       </div>
+    </>
   )
 }
 
