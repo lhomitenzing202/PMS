e2f26cc8b5dfb8d9a5970a81c0230a8d9bfd1845
lhomitenzing202 <tenzinglhomi15@gmail.com>
2023-12-12 18:41:55 +0545

    delete function with navigation added

--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -1,14 +1,28 @@
 
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import Navbar from "../../components/Navbar/Navbar"
 import "./SingleProduct.css"
 import axios from "axios"
 import { useEffect, useState } from "react"
 
 const SingleProduct =  () => {
+
+  const navigate = useNavigate()
      const {id} = useParams()
      //store data data coming in product
      const[product,setProduct] = useState({})
+
+     //delete product
+  const deleteProduct = async()=>{
+    const response = await axios.delete("https://65732380192318b7db419d5a.mockapi.io/Product/"+id)
+    if(response.status==200){
+      navigate("/")
+    }else{
+      "Something went wrong"
+    }
+  }
+ 
+   //fetch single product
     const fetchSingleProduct = async ()=>{
       const response = await axios.get("https://65732380192318b7db419d5a.mockapi.io/Product/"+id)
     setProduct(response.data)
@@ -25,7 +39,8 @@ const SingleProduct =  () => {
 <img src={product.ProductImg} alt="Product Image"/>
         <h2 className="product-name">{product.ProductName}</h2>
         <p className="product-description">{product.ProductDescription}</p>
-        <mark> {product.ProductMaterial}</mark>
+        <mark> {product.ProductMaterial}</mark><br/>
+        <button onClick={deleteProduct}>Delete</button>
         
         </div>
     </>
